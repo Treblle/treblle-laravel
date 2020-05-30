37e3e7477148c307c4896bbc0eccfc07ad20bb6f
cindreta <vedran@flip.hr>
2020-05-30 19:19:47 +0200

    Update Treblle.php

--- src/Treblle.php
+++ src/Treblle.php
@@ -73,7 +73,7 @@ class Treblle {
 
     public function terminate($request, $response) {
 
-        if(in_array(App::environment(), config('services.treblle.exclude'))) {
+        if(in_array(env('APP_ENV'), config('treblle.exclude'))) {
             exit;
         }
 
