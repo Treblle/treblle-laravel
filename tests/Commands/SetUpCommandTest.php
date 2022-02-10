50eabfda1f7daa69f3a7fd8fc8ed2fdbe3412161
Lucas Michot <lucas@semalead.com>
2022-02-10 14:53:06 +0100

    Using double quotes sometimes

--- tests/Commands/SetUpCommandTest.php
+++ tests/Commands/SetUpCommandTest.php
@@ -12,23 +12,20 @@ class SetUpCommandTest extends TestCase
 {
     public function testIfLookupRequestDoesntFindUserThenWeAllowUserToRegister(): void
     {
-        TreblleClient::fake(
-            [
-                TreblleClient::BASE_URL.'auth/lookup' => Http::response(['user' => null]),
-                TreblleClient::BASE_URL.'auth/register' => Http::response(['user' => ['uuid' => 'test', 'api_key' => 'test_key']]),
-                TreblleClient::BASE_URL.'projects/store' => Http::response(['project' => ['api_id' => 'test_id']]),
-            ]
-        );
+        TreblleClient::fake([
+            TreblleClient::BASE_URL.'auth/lookup' => Http::response(['user' => null]),
+            TreblleClient::BASE_URL.'auth/register' => Http::response(['user' => ['uuid' => 'test', 'api_key' => 'test_key']]),
+            TreblleClient::BASE_URL.'projects/store' => Http::response(['project' => ['api_id' => 'test_id']]),
+        ]);
 
-        $this
-            ->artisan('treblle:start')
-            ->expectsQuestion('📧 What\'s your email address?', 'test@test.test')
-            ->expectsQuestion('👨‍💻 What\'s your name?', 'Test')
-            ->expectsQuestion('🔒 Enter a new password for your account', 'password')
-            ->expectsQuestion('What\'s the name of your API project?', 'test')
-            ->expectsOutput('👏 Your project is ready! Add the following lines to your .env file and you are done!')
-            ->expectsOutput('TREBLLE_API_KEY=test_key')
-            ->expectsOutput('TREBLLE_PROJECT_ID=test_id')
-            ->assertExitCode(0);
+        $this->artisan('treblle:start')
+             ->expectsQuestion("📧 What's your email address?", 'test@test.test')
+             ->expectsQuestion("👨‍💻 What's your name?", 'Test')
+             ->expectsQuestion('🔒 Enter a new password for your account', 'password')
+             ->expectsQuestion("What's the name of your API project?", 'test')
+             ->expectsOutput('👏 Your project is ready! Add the following lines to your .env file and you are done!')
+             ->expectsOutput('TREBLLE_API_KEY=test_key')
+             ->expectsOutput('TREBLLE_PROJECT_ID=test_id')
+             ->assertExitCode(0);
     }
 }
