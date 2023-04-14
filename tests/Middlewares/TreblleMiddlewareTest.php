ac4a056666978f0d92c8ccdcc14bacec7425ca66
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-04-14 18:06:39 +0100

    chore: add test

--- tests/Middlewares/TreblleMiddlewareTest.php
+++ tests/Middlewares/TreblleMiddlewareTest.php
@@ -4,14 +4,28 @@ declare(strict_types=1);
 
 namespace Treblle\Tests\Middlewares;
 
+use Illuminate\Container\Container;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Facades\Http;
 use Treblle\Middlewares\TreblleMiddleware;
 use Treblle\Tests\TestCase;
+use Illuminate\Config\Repository as Config;
 
 class TreblleMiddlewareTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        $container = Container::setInstance(new Container());
+
+        Facade::setFacadeApplication($container);
+
+        $container->singleton('config', fn () => $this->createConfig());
+
+        parent::setUp();
+    }
+
     public function getMiddleware(): TreblleMiddleware
     {
         return $this->app->make(
@@ -35,5 +49,36 @@ class TreblleMiddlewareTest extends TestCase
         );
 
         Http::assertSentCount(1);
+    }
+
+    public function testIgnoredUrlNotMonitored()
+    {
+        Http::fake();
+
+        $this->getMiddleware()->handle(
+            request: Request::create(
+                uri: 'horizon',
+            ),
+            next: fn () => new Response(
+                content: json_encode($this->fixture(
+                    name: 'projects/create',
+                ), JSON_THROW_ON_ERROR)
+            ),
+        );
+
+        Http::assertNothingSent();
+    }
+
+    protected function createConfig(): Config
+    {
+        return new Config([
+            'treblle' => [
+                'ignore_urls' => [
+                    'telescope*',
+                    'horizon*',
+                    'nova*'
+                ],
+            ],
+        ]);
     }
 }
