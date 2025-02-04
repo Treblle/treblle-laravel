0513789161c11a58f989a904f851d827a1b54cd7
Bhushan <bhushangaykawad@gmail.com>
2025-02-04 12:23:52 +0530

    WIP

--- src/DataProviders/LaravelResponseDataProvider.php
+++ src/DataProviders/LaravelResponseDataProvider.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Treblle\DataProviders;
 
 use Treblle\FieldMasker;
+use Illuminate\Http\JsonResponse;
+use Treblle\DataTransferObject\Error;
+use Treblle\Contract\ErrorDataProvider;
 use Treblle\DataTransferObject\Response;
 use Treblle\Contract\ResponseDataProvider;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
@@ -13,18 +16,34 @@ final readonly class LaravelResponseDataProvider implements ResponseDataProvider
 {
     public function __construct(
         private FieldMasker     $fieldMasker,
-        private SymfonyResponse $response,
+        private JsonResponse|\Illuminate\Http\Response|SymfonyResponse $response,
+        private ErrorDataProvider                                      $errorDataProvider,
     ) {
     }
 
     public function getResponse(): Response
     {
+        $body = $this->response->getContent();
+        $size = mb_strlen($body);
+
+        if ($size > 2 * 1024 * 1024) {
+            $body = '{}';
+            $size = 0;
+
+            $this->errorDataProvider->addError(new Error(
+                message: 'JSON response size is over 2MB',
+                file: '',
+                line: 0,
+                type: 'E_USER_ERROR'
+            ));
+        }
+
         return new Response(
             code: $this->response->getStatusCode(),
-            size: mb_strlen($this->response->getContent()),
+            size: $size,
             load_time: microtime(true) - $this->startTime(),
             body: $this->fieldMasker->mask(
-                json_decode($this->response->getContent(), true) ?? []
+                json_decode($body, true) ?? []
             ),
             headers: $this->fieldMasker->mask(
                 collect($this->response->headers->all())->transform(
--- src/Middlewares/TreblleMiddleware.php
+++ src/Middlewares/TreblleMiddleware.php
@@ -6,11 +6,9 @@ namespace Treblle\Middlewares;
 
 use Closure;
 use Treblle\FieldMasker;
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Log;
 use Treblle\Factory\TreblleFactory;
 use Treblle\DataTransferObject\Error;
 use Treblle\InMemoryErrorDataProvider;
@@ -27,35 +25,11 @@ final class TreblleMiddleware
 
     public function terminate(Request $request, JsonResponse|Response|SymfonyResponse $response): void
     {
-        //        if (! $request->headers->has('X-TREBLLE-TRACE-ID')) {
-        //            $request->headers->add([
-        //                'X-TREBLLE-TRACE-ID' => $id = Str::uuid(),
-        //            ]);
-        //        }
-        //
-        //        $response->headers->add([
-        //            'X-TREBLLE-TRACE-ID' => $request->headers->get('X-TREBLLE-TRACE-ID'),
-        //        ]);
-        //
-        //        if (strlen((string) $response->getContent()) > 2 * 1024 * 1024) {
-        //            if (! app()->environment('production')) {
-        //                Log::error(
-        //                    message: 'Cannot send response over 2MB to Treblle.',
-        //                    context: [
-        //                        'url' => $request->fullUrl(),
-        //                        'date' => now()->toDateTimeString(),
-        //                    ]
-        //                );
-        //            }
-        //
-        //            return;
-        //        }
-
         $maskedFields = (array)config('treblle.masked_fields');
         $fieldMasker = new FieldMasker($maskedFields);
-        $requestProvider = new LaravelRequestDataProvider($fieldMasker, $request);
-        $responseProvider = new LaravelResponseDataProvider($fieldMasker, $response);
         $errorProvider = new InMemoryErrorDataProvider();
+        $requestProvider = new LaravelRequestDataProvider($fieldMasker, $request);
+        $responseProvider = new LaravelResponseDataProvider($fieldMasker, $response, $errorProvider);
 
         if (! empty($response->exception)) {
             $errorProvider->addError(new Error(
