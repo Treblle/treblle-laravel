c08fc8ca30ffef9a80f5c43a290f9732f2e2541a
Bhushan <bhushangaykawad@gmail.com>
2025-02-05 16:56:31 +0530

    WIP

--- src/TreblleServiceProvider.php
+++ src/TreblleServiceProvider.php
@@ -6,13 +6,9 @@ namespace Treblle\Laravel;
 
 use function config;
 use Illuminate\Routing\Router;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
-use Treblle\Laravel\Clients\TreblleClient;
-use Treblle\Laravel\Commands\SetupCommand;
 use Illuminate\Foundation\Console\AboutCommand;
 use Treblle\Laravel\Middlewares\TreblleMiddleware;
-use Treblle\Laravel\Contracts\TreblleClientContract;
 use Illuminate\Contracts\Container\BindingResolutionException;
 
 final class TreblleServiceProvider extends ServiceProvider
@@ -29,10 +25,6 @@ final class TreblleServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__ . '/../config/treblle.php' => config_path('treblle.php'),
             ], 'treblle-config');
-
-            //            $this->commands([
-            //                SetupCommand::class,
-            //            ]);
         }
 
         /** @var Router $router */
@@ -63,32 +55,5 @@ final class TreblleServiceProvider extends ServiceProvider
             path: __DIR__ . '/../config/treblle.php',
             key: 'treblle',
         );
-
-        //        $this->app->bind(
-        //            abstract: TreblleClientContract::class,
-        //            concrete: static function () {
-        //                $request = Http::baseUrl(
-        //                    url: 'https://app-api.treblle.com/v1/',
-        //                )->timeout(
-        //                    seconds: 15,
-        //                )->withHeaders([
-        //                    'Accept' => 'application/json',
-        //                    'Content-Type' => 'application/json',
-        //                    'User-Agent' => 'TreblleSetupCommand/0.1',
-        //                ]);
-        //
-        //                if (! empty(config('treblle.api_key'))) {
-        //                    $request->withHeaders(
-        //                        headers: [
-        //                            'x-api-key' => (string) config('treblle.api_key'),
-        //                        ]
-        //                    );
-        //                }
-        //
-        //                return new TreblleClient(
-        //                    request: $request,
-        //                );
-        //            },
-        //        );
     }
 }
